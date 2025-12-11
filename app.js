c13e53d500bacd007ba984ff54cf3d8cb1a43426
Jamie <jamiebuffing@gmail.com>
2025-12-11 14:41:28 +0100

    vercel hosting fix "database not initialized"

--- app.js
+++ app.js
@@ -15,14 +15,42 @@ if (!uri || !dbName) {
   process.exit(1);
 }
 
-// make db available later
-let db;
+// ----- DB connect helper -----
+let db = null;
+let dbPromise = null;
+
+async function getDb() {
+  // Als er al een db-verbinding is, gebruik die
+  if (db) return db;
+
+  // Als we al bezig zijn met verbinden, wacht daarop
+  if (dbPromise) return dbPromise;
+
+  // Nieuwe verbinding opzetten
+  const client = new MongoClient(uri);
+
+  dbPromise = client
+    .connect()
+    .then((client) => {
+      db = client.db(dbName);
+      console.log("Client connected to database:", dbName);
+      return db;
+    })
+    .catch((error) => {
+      console.error("Database connection error:", error);
+      // Als het misgaat, dbPromise resetten zodat we het later opnieuw kunnen proberen
+      dbPromise = null;
+      throw error;
+    });
+
+  return dbPromise;
+}
 
 // ----- Express setup -----
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "public/views"));
 
-// for POST /loginform
+// voor POST /loginform
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,6 +60,7 @@ app.use("/img", express.static(path.join(__dirname, "public/img")));
 app.use("/leafletcss", express.static(path.join(__dirname, "public/css/leaflet")));
 app.use("/leafletjs", express.static(path.join(__dirname, "public/js/leaflet")));
 
+// ----- Routes -----
 app.get("/", toonIndex);
 app.get("/login", toonLogin);
 
@@ -39,12 +68,17 @@ app.get("/api/keuzes", (req, res) => {
   res.json(keuzes);
 });
 
-app.get("/api/data", async (req, res) => {
-  const collection = db.collection("Data");
-  const data = await collection.find({}).toArray();
-  res.json(data);
+app.get("/api/data", async (req, res, next) => {
+  try {
+    const db = await getDb();
+    const collection = db.collection("Data");
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
-
 
 app.post("/loginform", (req, res) => {
   const loginAttempt = req.body;
@@ -53,12 +87,10 @@ app.post("/loginform", (req, res) => {
   res.render("pages/index");
 });
 
+// ----- Route handlers -----
 async function toonIndex(req, res, next) {
   try {
-    if (!db) {
-      return res.status(500).send("Database not initialized");
-    }
-
+    const db = await getDb();
     const collection = db.collection("Data");
     const points = await collection.find({}).toArray();
 
@@ -72,23 +104,6 @@ async function toonIndex(req, res, next) {
 function toonLogin(req, res) {
   res.render("pages/login");
 }
-
-
-
-// ----- DB connect -----
-async function connectDB() {
-  try {
-    const client = new MongoClient(uri);
-    await client.connect();
-    db = client.db(dbName);
-
-    console.log("Client connected to database:", dbName);
-  } catch (error) {
-    console.log("Database connection error:", error);
-  }
-}
-
-connectDB();
 
 // ----- Export / server start -----
 module.exports = app;
